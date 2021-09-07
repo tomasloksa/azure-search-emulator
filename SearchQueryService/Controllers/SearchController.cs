6b418c52f5729945b8e9dad90739c7c7c2bdcac0
Tomáš Lokša <loksa@kros.sk>
2021-09-07 16:39:08 +0200

    Add project files.

---
+++ SearchQueryService/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace SearchQueryService.Controllers
+{
+    [ApiController]
+    [Route("indexes/{indexName}/docs")]
+    public class SearchController : ControllerBase
+    {
+        private readonly HttpClient httpClient;
+        SearchController()
+        {
+            httpClient = new HttpClient();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(
+            [FromRoute]string indexName,
+            int top = 30,
+            int skip = 0,
+            string search = "",
+            string filter = "",
+            string searchMode = "",
+            string orderBy = ""
+        )
+        {
+            var rng = new Random();
+
+            httpClient.BaseAddress = new Uri("https://mocksearchresultsservice/searchQuery");
+
+            return Enumerable.Range(1, 1).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = indexName + " " + search
+            })
+            .ToArray();
+        }
+    }
+}
