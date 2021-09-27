fff6c68c98633aed9d357f15ef2aa2c96e932856
Tomáš Lokša <tomas.loksa@outlook.com>
2021-09-27 09:38:16 +0200

    cleanup

--- demo/SearchApi/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Performance", "RCS1198:Avoid unnecessary boxing of value type.", Justification = "<Pending>", Scope = "member", Target = "~M:SearchApi.Controllers.EshopItemsController.Search(System.Nullable{System.Int32},System.Nullable{System.Int32},System.String)~System.Threading.Tasks.Task{Microsoft.AspNetCore.Mvc.ContentResult}")]
